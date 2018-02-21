f10e8d6394493d1a0d91ceaa841d86e5828b5b82
paikwiki <paikwiki@gmail.com>
2018-02-21 21:52:02 +0900

    Write user test(undone):cold_sweat:

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -1,18 +1,28 @@
 <?php
+
 namespace Tests\Unit;
 
+use App\User;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class UserTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
      * User test
      *
      * @return void
      */
-    protected function setUp()
+
+    protected $user;
+
+    public function test_user_has_a_name()
     {
+        $this->user = factory(User::class)->create();
+//        dd($this->user);
+        // TODO: Write test!!
     }
 
 }
