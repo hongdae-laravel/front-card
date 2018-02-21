64f888399aab256848e8c81738b98473dbd9061b
paikwiki <paikwiki@gmail.com>
2018-02-21 21:18:55 +0900

    Add user test:family:

---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,18 @@
+<?php
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class UserTest extends TestCase
+{
+    /**
+     * User test
+     *
+     * @return void
+     */
+    protected function setUp()
+    {
+    }
+
+}
